ef9a3834fa11b03ed437654415a29738e085a0e2
Kyoko Sakuma <hzk.butterfly459@gmail.com>
2020-06-29 04:33:05 +0900

    日英切り替えまで

--- ui.js
+++ ui.js
@@ -2,4 +2,18 @@ $(document).ready(function(){
 	$('.button_good').on('click',function(e){
 		$('.button_good').children('img').attr('src', './img/button_redheart_noborder@2x.png');
 	});
+
+	$('.button_speak').modaal({
+		content_source: '#button_speak'
+	});
+
+	$('#switch1').click(function(){
+		if($('.caption_jp').css('display') == 'block'){
+			$('.caption_jp').css('display','none');
+			$('.caption_en').fadeIn();
+		}else{
+			$('.caption_en').css('display','none');
+			$('.caption_jp').fadeIn();
+		}
+    });
 })
