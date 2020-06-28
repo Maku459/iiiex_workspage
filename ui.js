3f5b8e509a50c1f879d14471523987744a21714c
Kyoko Sakuma <hzk.butterfly459@gmail.com>
2020-06-29 05:10:16 +0900

    時間帯による分岐

--- ui.js
+++ ui.js
@@ -15,5 +15,16 @@ $(document).ready(function(){
 			$('.caption_en').css('display','none');
 			$('.caption_jp').fadeIn();
 		}
-    });
+	});
+	
+	var now_hour = new Date().getHours();
+    if ( 10 <= now_hour && now_hour <= 18 ){
+		$('.button_speak').children('img').attr('src', './img/button_sf2_active@2x.png');
+		$('.speak_inactive').css('display','none');
+		$('.speak_active').css('display','block');
+    } else {
+		$('.button_speak').children('img').attr('src', './img/button_sf2_inactive@2x.png');
+		$('.speak_active').css('display','none');
+		$('.speak_inactive').css('display','block');
+    }
 })
